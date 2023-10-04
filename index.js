478a87b98f2025e2b99a443beb8aa6c60480f49b
Mujahed Twair <135132989+MujahedTwair@users.noreply.github.com>
2023-10-04 18:33:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ dotenv.config();
 import  express  from "express";
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
