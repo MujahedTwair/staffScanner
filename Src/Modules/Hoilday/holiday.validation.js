d4884aecbe54ca2d47100f5137ca407161ccb290
MujahedTwair <mojad.tweir@gmail.com>
2023-11-05 19:54:11 +0200

    holiday validation

--- Src/Modules/Hoilday/holiday.validation.js
+++ Src/Modules/Hoilday/holiday.validation.js
@@ -1,15 +1,25 @@
 import joi from 'joi';
+import { DateTime } from 'luxon';
 
 export const requestHolidaySchema = {
     body: joi.object({
-        startDate: joi.string().required(),
-        endDate: joi.string().required(),
-        type: joi.string().valid('Sick', 'Vacation', 'Travelling').required(),
-        paid: joi.boolean().required(),
-        reason: joi.string()
+      startDate: joi.string().required(),
+      endDate: joi.string().required(),
+      type: joi.string().valid('Sick', 'Vacation', 'Travelling').required(),
+      paid: joi.boolean().required(),
+      reason: joi.string(),
+    }).custom((value, helpers) => {
+      const startDate = DateTime.fromFormat(value.startDate, 'd/M/yyyy');
+      const endDate = DateTime.fromFormat(value.endDate, 'd/M/yyyy');
+      const now = DateTime.now().setZone('Asia/Jerusalem').startOf('day');
+      console.log(now);
+      if (startDate.isValid && endDate.isValid && startDate >= now && endDate >= startDate) {
+        return value;
+      } else {
+        return helpers.error('End date must be a valid date and after the start date and not in the past');
+      }
     }),
-
-};
+  };
 
 export const approveHolidaySchema = {
     body: joi.object({
